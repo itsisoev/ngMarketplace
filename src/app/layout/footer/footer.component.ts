ff5ffb6b4b4c69d2d7ab1c7710a7f33e3c949df2
Zuhra M <zm544052@gmail.com>
2025-06-21 22:35:48 +0500

    add footer translation

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, signal } from '@angular/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+
+interface ILanguages {
+  code: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
@@ -13,4 +19,35 @@ export class FooterComponent {
   storePhone = signal<string>('(629) 555-0129');
   storeAddress = signal<string>('4517 Washington Ave. Manchester, Kentucky 39495');
   storeEmail = signal<string>('info@kinbo.com');
+    private translate = inject(TranslateService);
+
+  languages = signal<ILanguages[]>([
+    { code: 'en', name: 'En' },
+    { code: 'ru', name: 'Ru' },
+    { code: 'tj', name: 'Tj' }
+  ])
+
+  currentLang = signal<string>(this.getSavedLang());
+
+  constructor() {
+    effect(() => {
+      const lang = this.currentLang();
+      this.translate.use(lang);
+      localStorage.setItem('language', lang);
+    });
+    this.translate.use(this.currentLang());
+  }
+
+  private getSavedLang(): string {
+    return localStorage.getItem('language') ||
+      this.translate.getBrowserLang() ||
+      'en';
+  }
+
+  changeLanguage(event: Event): void {
+    const select = event.target as HTMLSelectElement;
+    this.currentLang.set(select.value);
+  }
+
+
 }
